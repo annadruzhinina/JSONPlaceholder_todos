9e0fead91a7babee3414101541123491ef7aacaf
anna <anna@rcbd.org>
2023-09-06 13:23:54 -0400

    JSON Placeholder с ручкой (endpoint) «todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch("https://jsonplaceholder.typicode.com/todos/?_limit=100'")
+      .then((loadedData) => loadedData.json())
+      .then((loadedTodos) => {
+        setTodos(loadedTodos);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todos List</h1>
+      {isLoading ? (
+        <div className="loader">...</div>
+      ) : (
+        <div className="table-container">
+          <table className="centered-table">
+            <thead>
+              <tr>
+                <th>UserID</th>
+                <th>Title</th>
+                <th>Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todos.map(({ userId, id, title, completed }) => (
+                <tr key={id}>
+                  <td>{userId}</td>
+                  <td>{title}</td>
+                  <td>{completed ? "Yes" : "No"}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
